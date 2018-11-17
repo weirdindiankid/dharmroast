31db027a243b23367e8daff2ff1e0e346ad96807
Dharmesh Tarapore <dharmesh@bu.edu>
2018-11-17 03:42:00 -0500

    Updates to stuff.

--- proto/proto.py
+++ proto/proto.py
@@ -4,6 +4,12 @@
 import praw
 import secrets as secrets
 from imagedownloader import *
+import mysql.connector
+
+mydb = mysql.connector.connect(user=secrets.MYSQL_USERNAME, passwd=secrets.MYSQL_PASSWORD,
+    database=secrets.MYSQL_DATABASE, host=secrets.MYSQL_HOST)
+
+mycursor = mydb.cursor()
 
 REDDIT_USERNAME = secrets.REDDIT_USERNAME
 REDDIT_PASSWORD = secrets.REDDIT_PASSWORD
@@ -11,13 +17,13 @@ IMAGE_DIR = 'images/'
 
 image_downloader = ImageDownloader()
 
-DEBUG = True
+DEBUG = False
 NUMBER_OF_COMMENTS = 5
 
 if DEBUG:
     NUMBER_OF_POSTS = 5
 else:
-    NUMBER_OF_POSTS = 25
+    NUMBER_OF_POSTS = 250
 
 reddit = praw.Reddit(client_id='9KimOyjWFOM8ag',
                      client_secret='q11CnnYVwTJ7P927XKYJHJi_Rvg',
@@ -43,9 +49,20 @@ for submission in roastme.top(limit=NUMBER_OF_POSTS):
             # TODO: Also figure out if the retrieved images contain faces of humans/potatoes.
             print(submission.title)
             comments = submission.comments
-            for comment in comments:
-                print(comment.body)
+            comments_list = []
+            for comment in comments[:6]:
+                comments_list.append(comment.body)
 
+            print(comments_list[5])
+            #mydb.cursor().execute("INSERT INTO roasts (image_link, post_title, c1, c2, c3, c4, c5, c6) VALUES(\"a\", \"b\", \"c\", \"d\", \"e\", \"f\", \"g\", \"h\") ")
+            sql = "INSERT INTO roasts (image_link, post_title, c1, c2, c3, c4, c5, c6) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            vals = (image_file_name, title, comments_list[0], comments_list[1], comments_list[2],
+                comments_list[3], comments_list[4], comments_list[5])
+            # rc = mydb.cursor()
+            # rc.execute(sql, values)
+            # rc.commit()
+            mycursor.execute(sql, vals)
+            mydb.commit()
         except Exception as e:
             # TODO: Write this to a log file.
             with open("error.log", "a+") as error_log_file:
